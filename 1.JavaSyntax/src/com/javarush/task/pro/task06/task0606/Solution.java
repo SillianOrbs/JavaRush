c57b2da3e89b6db1b884fda2224d21ec93a78916
Nai <88776801+SillianOrbs@users.noreply.github.com>
2022-02-07 20:33:49 +0300

    7 уровнь в JavaRush - часть задач, дол задачи с несколькими этапами решений

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0606/Solution.java
@@ -0,0 +1,54 @@
+package com.javarush.task.pro.task06.task0606;
+
+/* 
+Универсальный солдат
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void universalMethod(){
+
+    }
+
+    //напишите тут ваш код
+
+    public static void universalMethod(int arg){
+
+    }
+
+    public static void universalMethod(String arg){
+
+    }
+
+    public static void universalMethod(double arg){
+
+    }
+
+    public static void universalMethod(boolean arg){
+
+    }
+
+    public static void universalMethod(boolean arg1, String arg2){
+
+    }
+
+    public static void universalMethod(boolean arg1, String arg2, int arg3){
+
+    }
+
+    public static void universalMethod(String arg2, int arg3){
+
+    }
+
+    public static void universalMethod(String arg2, int arg3, boolean arg1){
+
+    }
+
+    public static void universalMethod(int arg3, boolean arg1){
+
+    }
+}
