dd7cd553fbc023076ebbb38c5ae0b30b6ce95bc6
Nai <88776801+SillianOrbs@users.noreply.github.com>
2022-02-09 16:27:24 +0300

    8 уровнь в JavaRush - вторая часть этапной задачи

--- 1.JavaSyntax/src/com/javarush/task/pro/task07/task0720/GalavirXIII.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0720/GalavirXIII.java
@@ -2,6 +2,7 @@ package com.javarush.task.pro.task07.task0720;
 
 public class GalavirXIII {
     public static int superWeapon = Integer.MAX_VALUE;
+
     public double health = Double.MAX_VALUE;
 
     public void defend(double x) {
